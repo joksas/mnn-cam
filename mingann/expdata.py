a08a74271ab8a883b1f8a00576825e2ce9193d93
Dovydas Joksas <dovydas@yoshke.org>
2021-10-22 14:05:57 +0100

    load retention voltages

--- mingann/expdata.py
+++ mingann/expdata.py
@@ -4,8 +4,19 @@ import os
 from pathlib import Path
 
 
-def load_retention():
+def load_retention_table():
     path = os.path.join(Path(__file__).parent.parent.absolute(), "expdata", "32-levels-retention.xlsx")
     whole_table = pd.read_excel(path)
-    retention_data = np.array(whole_table.iloc[:1000, 3:])
-    return retention_data
+    return whole_table
+
+
+def load_retention_currents():
+    retention_currents = load_retention_table().iloc[:1000, 3:]
+    retention_currents = np.array(retention_currents)
+    return retention_currents
+
+
+def load_retention_voltages():
+    retention_voltages = load_retention_table().iloc[:1000, 1]
+    retention_voltages = np.array(retention_voltages)
+    return retention_voltages
