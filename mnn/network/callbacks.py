b89f09a8d997d7226bf0cf62043ea1efd4e3011b
Dovydas Joksas <dovydas@yoshke.org>
2022-08-01 15:03:27 +0300

    improve printing callback

--- mnn/network/callbacks.py
+++ mnn/network/callbacks.py
@@ -4,10 +4,7 @@ from tensorflow import keras
 
 
 class TrainingLogging(keras.callbacks.Callback):
-    def on_epoch_begin(self, epoch, logs=None):
-        logging.info(f"Epoch {epoch+1}/{self.params['epochs']}")
-
     def on_epoch_end(self, epoch, logs=None):
         logging.info(
-            f"loss: {logs['loss']:.4f} - accuracy: {logs['accuracy']:.4f} - val_loss: {logs['val_loss']:.4f} - val_accuracy: {logs['val_accuracy']:.4f}"
+            f"Epoch {epoch+1}/{self.params['epochs']}\tloss: {logs['loss']:.4f} - accuracy: {logs['accuracy']:.4f} - val_loss: {logs['val_loss']:.4f} - val_accuracy: {logs['val_accuracy']:.4f}"
         )
